b80fd57b762add8b918908e65546925d41238c47
migue <migue@LAPTOP-HT4P40ID>
2021-12-18 17:13:17 +0100

    Añadimos el proyecto base

---
+++ GestorCovid/src/main/java/B02/GestorCovid/App.java
@@ -0,0 +1,13 @@
+package B02.GestorCovid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
