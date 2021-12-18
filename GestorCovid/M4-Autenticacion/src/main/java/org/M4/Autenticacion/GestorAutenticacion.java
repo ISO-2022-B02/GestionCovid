dbe08f7904799903cc883d6f2e9af839403ea396
migue <migue@LAPTOP-HT4P40ID>
2021-12-18 18:31:28 +0100

    Añadimos el módulo 4 al proyecto

---
+++ GestorCovid/M4-Autenticacion/src/main/java/org/M4/Autenticacion/GestorAutenticacion.java
@@ -0,0 +1,8 @@
+package org.M4.Autenticacion;
+
+public class GestorAutenticacion{
+
+	public void ComprobarLogin(String aLogin, String aPassword) {
+		throw new UnsupportedOperationException();
+	}
+}
