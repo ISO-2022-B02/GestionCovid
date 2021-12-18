4420281cfee62be35d17c855fb6ede89e698cfca
migue <migue@LAPTOP-HT4P40ID>
2021-12-18 18:24:04 +0100

    Añadimos el módulo 3

---
+++ GestorCovid/M3-Estadisticas/src/main/java/org/M3/Estadisticas/App.java
@@ -0,0 +1,13 @@
+package org.M3.Estadisticas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GestorCovid/M3-Estadisticas/src/main/java/org/M3/Estadisticas/GestorEstadisticas.java
@@ -0,0 +1,12 @@
+package org.M3.Estadisticas;
+
+public class GestorEstadisticas {
+
+	public String[] RealizarSentenciaSQL() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void Procesar_informacion() {
+		throw new UnsupportedOperationException();
+	}
+}
