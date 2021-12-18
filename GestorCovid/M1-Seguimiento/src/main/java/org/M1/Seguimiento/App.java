f4218782db6482b8a710e2b11f4a485a0b729827
migue <migue@LAPTOP-HT4P40ID>
2021-12-18 17:29:44 +0100

    Añadimos el módulo 1 al proyecto

---
+++ GestorCovid/M1-Seguimiento/src/main/java/org/M1/Seguimiento/App.java
@@ -0,0 +1,13 @@
+package org.M1.Seguimiento;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
